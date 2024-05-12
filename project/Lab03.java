2eee2abee3c91b8f96d79cc75001f0c66e3195f4
WolfGang011 <thaiphong011199@gmail.com>
2024-05-12 21:05:26 +0700

    thuc hanh

--- project/Lab03.java
+++ project/Lab03.java
@@ -1,5 +1,18 @@
 package project;
 
-public class Lab03 {
+import java.util.Scanner;
 
+public class Lab03 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Nhập giá trị cạnh lập phương : ");
+		int canh = scanner.nextInt();
+
+		double thetich = Math.pow(canh, 3);
+
+		System.out.println("Thể tích : " + thetich);
+
+		scanner.close();
+	}
 }
---
+++ project/datatype.java
@@ -0,0 +1,10 @@
+package project;
+
+public class datatype {
+	public static void main(String[] args) {
+		byte a = 100;
+		short b = 10000;
+		long c = 50000L;
+		float e = 400f;
+	}
+}
